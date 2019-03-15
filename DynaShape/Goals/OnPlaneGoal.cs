94e89e0d0866aa017cd2dc7bade44bddf5360efc
Long Nguyen <longnguyen.connect@gmail.com>
2019-03-15 23:06:35 +0100

    FIxex bug in OnPlaneGoal

--- DynaShape/Goals/OnPlaneGoal.cs
+++ DynaShape/Goals/OnPlaneGoal.cs
@@ -9,7 +9,14 @@ namespace DynaShape.Goals
     public class OnPlaneGoal : Goal
     {
         public Triple TargetPlaneOrigin;
-        public Triple TargetPlaneNormal;
+
+        public Triple TargetPlaneNormal
+        {
+            get => targetPlaneNormal;
+            set { targetPlaneNormal = value.Normalise(); }
+        }
+
+        private Triple targetPlaneNormal;
 
 
         public OnPlaneGoal(List<Triple> nodeStartingPositions, Triple planeOrigin, Triple planeNormal, float weight = 1f)
@@ -33,7 +40,7 @@ namespace DynaShape.Goals
         {
             for (int i = 0; i < NodeCount; i++)
             {
-                Moves[i] = TargetPlaneNormal * -TargetPlaneNormal.Dot(allNodes[NodeIndices[i]].Position);
+                Moves[i] = TargetPlaneNormal * -TargetPlaneNormal.Dot(allNodes[NodeIndices[i]].Position - TargetPlaneOrigin);
                 Weights[i] = Weight;
             }
         }
