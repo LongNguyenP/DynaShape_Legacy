56684e07ff3fd1a8c67a90d30bc98f3717a80325
Long Nguyen <longnguyen.connect@gmail.com>
2020-03-23 22:32:25 +0100

    Important

--- DynaShape/Goals/OnPlaneGoal.cs
+++ DynaShape/Goals/OnPlaneGoal.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using Autodesk.DesignScript.Geometry;
 using Autodesk.DesignScript.Runtime;
--- DynaShape/Goals/ShapeMatchingGoal.cs
+++ DynaShape/Goals/ShapeMatchingGoal.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Autodesk.DesignScript.Runtime;
+
 
 namespace DynaShape.Goals
 {
